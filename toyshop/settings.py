a1350762a0ffe85242571cb747caaa54f2a60b2b
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2022-04-29 21:03:13 +0000

    added mobile_header and main navbar

--- toyshop/settings.py
+++ toyshop/settings.py
@@ -145,16 +145,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'

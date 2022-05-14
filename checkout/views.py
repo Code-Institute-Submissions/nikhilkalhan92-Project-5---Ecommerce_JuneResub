04a7e2a23a22f82d2f888f5f7a50039451137b0d
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2022-05-14 12:12:05 +0000

    added striple elements to checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ks63zCJXw5Xd4JiFfizLQcfjznWgzdARXeYbRhsZNipSe2gM7Ku9f12dGrLz57ZYmo41GJQHx6B7YNilbBE1SRW00BZRfBksO',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

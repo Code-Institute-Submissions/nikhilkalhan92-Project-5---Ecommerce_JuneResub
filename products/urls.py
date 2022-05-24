43becfb1e5d9b283783c09464caeb8b012c87395
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2022-05-24 18:35:57 +0000

    admin - usr can edit or delete product from homepage

--- products/urls.py
+++ products/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('update/<int:pk>/',views.update,name='update'),
     path('delete/<int:pk>/',views.delete,name='delete'),
     path('add/', views.add_product, name='add_product'),
-     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+
 ]

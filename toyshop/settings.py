0b7c2b5ac857783c34cece779e281443ee0928f8
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2022-06-26 15:16:15 +0000

    changed debug and email verifcation settings to recieve when signed up

--- toyshop/settings.py
+++ toyshop/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
 ALLOWED_HOSTS = ['project5-ecommerce.herokuapp.com','localhost']
 
@@ -111,7 +111,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -219,5 +218,15 @@ DEFAULT_FROM_EMAIL = 'nikstoyshop@example.com'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-ACCOUNT_EMAIL_VERIFICATION = "none"
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'nikstoyshop@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
